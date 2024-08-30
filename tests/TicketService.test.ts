812eb636e3cb11cc5f49d15badb7069dba9744e9
uncpleo <leonardocqtc@gmail.com>
2024-08-30 13:52:23 -0500

    Actualizacion para el despliegue 2.0

--- tests/TicketService.test.ts
+++ tests/TicketService.test.ts
@@ -13,15 +13,20 @@ describe('TicketService', () => {
 
   it('should allow a valid ticket to be purchased', async () => {
     const lines = [new Line([1, 2, 3, 4, 5], 10)];
-    const ticket = new Ticket('123', new Date(), lines);
+    const ticket = new Ticket('123', new Date(Date.now() - 100 * 60 * 1000), lines);
     const result = await ticketService.requestTicketPurchase(ticket);
     expect(result).toBe(true);
   });
 
   it('should throw an error if the ticket has more than 5 lines', async () => {
     const lines = Array(6).fill(new Line([1, 2, 3, 4, 5], 10));
-    const ticket = new Ticket('123', new Date(), lines);
+    const ticket = new Ticket('123', new Date(Date.now() - 100 * 60 * 1000), lines); 
     await expect(ticketService.requestTicketPurchase(ticket)).rejects.toThrow("A ticket cannot have more than 5 lines.");
   });
 
+  it('should throw an error if the ticket is purchased less than 60 minutes before the drawing event', async () => {
+    const lines = [new Line([1, 2, 3, 4, 5], 10)];
+    const ticket = new Ticket('123', new Date(), lines);
+    await expect(ticketService.requestTicketPurchase(ticket)).rejects.toThrow("Ticket must be purchased at least 60 minutes before the drawing event.");
+  });
 });
